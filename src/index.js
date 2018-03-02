59dc86820b2165614389242ca8c03cc6c928c3fb
poulkud <poulkud@gmail.com>
2018-03-02 18:56:18 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var rem = number;
+  var sum = 0;
+  while(rem = Math.floor(rem / 5)){
+    sum = sum + rem;
+  }
+  return sum;
 }
